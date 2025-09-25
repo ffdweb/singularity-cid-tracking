d08f1af899fea5eac253dd62938ab45853259015
Brian <brianeggert@prelingerarchives.online>
2025-09-25 14:44:09 -0500

    Create db_prep_report.sh
    adding prep report script to load preparations table

---
+++ db_prep_report.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Usage: ./db_prep_report.sh
+# Output is saved to preparations.csv with columns: PrepID,Name,StorageID
+
+output_file="preparations.csv"
+
+# Truncate file and write header
+echo "PrepID,Name,StorageID" > "$output_file"
+
+# Get the full prep list from Singularity
+singularity prep list | awk '
+    /^[0-9]+[[:space:]]/ {
+        # First line of a prep block, grab ID and Name
+        prep_id=$1
+        name=$2
+        for (i=3; i<=NF; i++) name=name" "$i
+    }
+    /^[[:space:]]+[0-9]+[[:space:]]/ {
+        # Storage block line, grab StorageID
+        storage_id=$1
+        printf "%s,%s,%s\n", prep_id, name, storage_id
+    }
+' >> "$output_file"
+
+echo "Output written to $output_file"
