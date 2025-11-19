3e079147c5fa74676bc7e005da07058b3d06dd7f
Brian <brianeggert@prelingerarchives.online>
2025-11-19 17:22:17 -0600

    Update raw-imports.sql

--- raw-imports.sql
+++ raw-imports.sql
@@ -20,3 +20,15 @@ CREATE TABLE storages_raw_basic (
     StorageType    TEXT,
     StoragePath    TEXT
 );
+
+CREATE TABLE deals_raw (
+    DealID        BIGINT PRIMARY KEY,
+    State         TEXT NOT NULL,
+    Provider      TEXT NOT NULL,
+    PieceCID      TEXT NOT NULL,
+    PieceSize     BIGINT NOT NULL,
+    StartEpoch    BIGINT NOT NULL,
+    Price         BIGINT NOT NULL,
+    Verified      BOOLEAN NOT NULL,
+    ClientID      TEXT NOT NULL
+);
