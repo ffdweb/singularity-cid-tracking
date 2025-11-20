071d9bf1676a44b321bf3432afbb4d3f352489da
Brian <brianeggert@prelingerarchives.online>
2025-11-20 13:16:00 -0600

    Update raw-imports.sql

--- raw-imports.sql
+++ raw-imports.sql
@@ -55,3 +55,23 @@ CREATE TABLE deals_full (
     LastUpdatedEpoch      BIGINT,
     SlashEpoch            BIGINT
 );
+
+CREATE TABLE collection_35mmstockfootage_raw (
+    identifier TEXT PRIMARY KEY
+);
+
+
+\copy collection_35mmstockfootage_raw FROM '/users/brianeggert/35mmstockfootage-identifiers.txt' WITH (FORMAT text);
+-- collection_35mmstockfootage_raw 1602
+-- collection_legacy_prelinger_raw 8710
+-- collection_aruba_raw 182638
+-- us_fbis_raw 16293
+-- rohingya_archive_raw 9
+-- eot_2024_pre_election_raw 13635
+-- eot_2024_interim_raw 27051
+-- eot_2024_post_inauguration_raw 28306
+-- archiveteam_eot2024_usgovernment_raw 80765
+-- archiveteam_eot2024_voiceofamerica_raw 36367
+-- eot_2024_unt_raw 1577
+-- eot_2024_webrecorder_raw 386
+
