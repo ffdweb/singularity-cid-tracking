253b445f8e542439efee69c9128a11e3c83f3fb4
Brian <brianeggert@prelingerarchives.online>
2026-01-12 15:50:05 -0600

    Update queries.sql

--- user/queries.sql
+++ user/queries.sql
@@ -1,1 +1,19 @@
+-- preps fully sealed by provider
 
+SELECT p.PrepID,
+       p.Name
+FROM preparations p
+JOIN (
+    SELECT pc.PrepID,
+           COUNT(*) AS total_pieces,
+           COUNT(DISTINCT d.PieceCID) AS pieces_with_active_deal
+    FROM pieces pc
+    LEFT JOIN deals d
+        ON pc.PieceCID = d.PieceCID
+        AND d.Provider = 'f02011071'
+        AND d.State = 'active'
+    GROUP BY pc.PrepID
+) stats
+  ON p.PrepID = stats.PrepID
+WHERE stats.total_pieces = stats.pieces_with_active_deal
+ORDER BY p.PrepID ASC;
