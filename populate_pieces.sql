07a776b321f8c7a36d5f53072dbfe637c9d39c80
Brian <brianeggert@prelingerarchives.online>
2026-01-12 12:39:46 -0600

    Create populate_pieces.sql

---
+++ populate_pieces.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS pieces_staging;
+
+
+CREATE TABLE pieces_staging (
+    PieceCID    TEXT NOT NULL,
+    PieceSize   BIGINT NOT NULL,
+    RootCID     TEXT NOT NULL,
+    FileSize    BIGINT NOT NULL,
+    StorageID   BIGINT NOT NULL,
+    PrepID      BIGINT NOT NULL,
+    IsDag       BOOLEAN NOT NULL
+);
+
+
+\copy pieces_staging (PieceCID, PieceSize, RootCID, FileSize, StorageID, PrepID, IsDag) FROM '/Users/brianeggert/workscripts/postgres/pieces.csv' WITH (FORMAT csv, HEADER true)
+
+
+INSERT INTO pieces
+SELECT *
+FROM pieces_staging
+ON CONFLICT (PieceCID, PrepID, StorageID) DO NOTHING;
+
+
+-- check which rows were excluded
+SELECT ps.*
+FROM pieces_staging ps
+LEFT JOIN pieces p
+  ON ps.PieceCID  = p.PieceCID
+ AND ps.PrepID    = p.PrepID
+ AND ps.StorageID = p.StorageID
+WHERE p.PieceCID IS NOT NULL;
