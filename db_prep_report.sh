87b3ba66a50c8575387c400153923ea195830b15
Brian <brianeggert@prelingerarchives.online>
2026-01-12 13:01:51 -0600

    Update db_prep_report.sh

--- db_prep_report.sh
+++ db_prep_report.sh
@@ -4,22 +4,27 @@
 # Output is saved to preparations.csv with columns: PrepID,Name,StorageID
 
 output_file="preparations.csv"
+temp_file="$(mktemp)"
 
-# Truncate file and write header
+# Truncate output and write header
 echo "PrepID,Name,StorageID" > "$output_file"
 
-# Get the full prep list from Singularity
+# Get the full prep list from Singularity and process with awk
 singularity prep list | awk '
-    /^[0-9]+[[:space:]]/ {
+    /^[0-9][0-9]*[[:space:]]/ {
         # First line of a prep block, grab ID and Name (only $2, not the rest)
         prep_id=$1
         name=$2
     }
-    /^[[:space:]]+[0-9]+[[:space:]]/ {
+    /^[[:space:]][[:space:]]*[0-9][0-9]*[[:space:]]/ {
         # Storage block line, grab StorageID
         storage_id=$1
         printf "%s,%s,%s\n", prep_id, name, storage_id
     }
-' >> "$output_file"
+' > "$temp_file"
+
+# Sort by numeric PrepID and append to output file
+sort -t, -k1,1n "$temp_file" >> "$output_file"
+rm "$temp_file"
 
 echo "Output written to $output_file"
