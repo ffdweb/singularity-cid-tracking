58ab1d50cd485da657db3a41c4de7525e0a3e3eb
Brian <brianeggert@prelingerarchives.online>
2026-01-12 12:53:59 -0600

    Update construct-db.sql

--- construct-db.sql
+++ construct-db.sql
@@ -1,20 +1,3 @@
---CLIENTS
-
-CREATE TABLE clients (
-    ClientID      VARCHAR(20) PRIMARY KEY,  -- f0...
-    ClientAddress TEXT NOT NULL,            -- f1...
-    Active        BOOLEAN NOT NULL,         -- true/false
-    ClientOwner   VARCHAR(10) NOT NULL      -- 'PA' or 'IA'
-);
-
-
-INSERT INTO clients (ClientID, ClientAddress, Active, ClientOwner) VALUES
-('f02208630', 'f1i2reokfclrqls5mkgtbqk5esvj6it7nykv7c57y', false,  'PA'),
-('f03091977', 'f1hnvljphtrpwb6pxszxoh7k57br7goo33s6b22ry', true,  'PA'),
-('f01131298', 'f1wp6zoxj7sydnrywvzp276x3gayghi7r6le4tcwy', false, 'IA'),
-('f03510418', 'f1skjgotikvvlx3uzhzltiq2ejmwdmtxt5doa5vpy', true, 'IA');
-
-
 -- PIECES
 
 CREATE TABLE pieces (
