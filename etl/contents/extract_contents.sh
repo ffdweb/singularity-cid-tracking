c6ac9d7cfb77b40ce702291f4bda717b64303046
brianeggert <brianeggert@prelingerarchives.online>
2026-01-13 16:25:43 -0600

    naming

--- etl/contents/extract_contents.sh
+++ etl/contents/extract_contents.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-START=402
-END=403
-OUTFILE="items-storages.csv"
+START=500
+END=1000
+OUTFILE="storage-contents.csv"
 
 echo "storageid,identifier" > "$OUTFILE"
 
 for storage in $(seq "$START" "$END"); do
-    singularity storage explore "$storage" 2>/dev/null \
+    timeout 10s singularity storage explore "$storage" 2>/dev/null \
         | awk -v sid="$storage" '
-            NR == 1 { next }                 # skip header
-            NF == 0 { next }                 # skip empty lines
+            NR == 1 { next }
+            NF == 0 { next }
             $1 ~ /_meta\.xml$/ {
                 id = $1
                 sub(/_meta\.xml$/, "", id)
                 if (!seen[id]++) print sid "," id
             }
         ' >> "$OUTFILE"
+
+    if [ $? -eq 124 ]; then
+        echo "storage $storage timed out" >&2
+    fi
 done
