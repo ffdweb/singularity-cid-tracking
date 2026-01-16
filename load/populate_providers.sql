dbad1ef3d76244b14591323fb271da1927c86e5c
Brian <brianeggert@prelingerarchives.online>
2026-01-16 15:26:33 -0600

    Create populate_providers.sql

---
+++ load/populate_providers.sql
@@ -0,0 +1,10 @@
+CREATE TABLE providers (
+    ProviderID    Text NOT NULL
+    ProviderName  TEXT NOT NULL
+);
+
+
+INSERT INTO providers (ProviderID, ProviderName) VALUES
+('f02011071', 'tillie'),
+('f02639429', 'milad'),
+('f03468521', 'dcent');
